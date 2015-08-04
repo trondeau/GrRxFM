f14e617a76a7dbdb1e5a716057b7fe6803c1939d
Tom Rondeau <tom@trondeau.com>
2015-08-04 19:25:37 -0400

    Adding ControlPort c&c over setting of freq and gain.
    Requires update to gr-uhd.
    
    Sliders on main app work fine; sliders on rungraph have network
    threading issue by being run in the main thread. Need to offload this
    somehow.

--- app/src/main/jni/fg.cpp
+++ app/src/main/jni/fg.cpp
@@ -4,18 +4,23 @@
 
 // for gnuradio
 #include <gnuradio/top_block.h>
+#include <gnuradio/blocks/nop.h>
 #include <gnuradio/filter/firdes.h>
 #include <gnuradio/filter/interp_fir_filter_fff.h>
 #include <gnuradio/analog/frequency_modulator_fc.h>
+#include <gnuradio/uhd/usrp_sink.h>
 #include <grand/opensl_source.h>
-#include <osmosdr/sink.h>
+//#include <osmosdr/sink.h>
 
 JavaVM *vm;
 gr::top_block_sptr tb;
 gr::grand::opensl_source::sptr src;
 gr::filter::interp_fir_filter_fff::sptr interp;
 gr::analog::frequency_modulator_fc::sptr fm;
-osmosdr::sink::sptr snk;
+//osmosdr::sink::sptr snk;
+gr::uhd::usrp_sink::sptr snk;
+
+gr::blocks::nop::sptr nop;
 
 extern "C" {
 
@@ -26,13 +31,15 @@ Java_org_gnuradio_grtxfm_RunGraph_FgInit(JNIEnv* env, jobject thiz,
   GR_INFO("fg", "FgInit Called");
 
   float samp_rate = 32e3;
-  float fc = 444e6;
-  float gain = 60;
 
   const char *c_devname = env->GetStringUTFChars(devname, NULL);
   std::stringstream args;
   args << "uhd,fd=" << fd << ",uspfs_path=" << c_devname;
   GR_INFO("fg", boost::str(boost::format("Using UHD args=%1%") % args.str()));
+
+  uhd::stream_args_t stream_args;
+  stream_args.cpu_format = "fc32";
+  stream_args.otw_format = "sc16";
 
   int resamp = 10;
   std::vector<float> taps = gr::filter::firdes::low_pass_2(resamp, resamp*samp_rate, 10e3, 2e3, 60,
@@ -45,14 +52,16 @@ Java_org_gnuradio_grtxfm_RunGraph_FgInit(JNIEnv* env, jobject thiz,
   src = gr::grand::opensl_source::make(int(samp_rate));
   interp = gr::filter::interp_fir_filter_fff::make(resamp, taps);
   fm = gr::analog::frequency_modulator_fc::make(k);
-  snk = osmosdr::sink::make(args.str());
-  snk->set_sample_rate(resamp*samp_rate);
-  snk->set_center_freq(fc);
-  snk->set_gain(gain);
+  //snk = osmosdr::sink::make(args.str());
+  snk = gr::uhd::usrp_sink::make(args.str(), stream_args);
+  snk->set_samp_rate(resamp*samp_rate);
+
+  nop = gr::blocks::nop::make(sizeof(float));
 
   tb->connect(src, 0, interp, 0);
   tb->connect(interp, 0, fm, 0);
   tb->connect(fm, 0, snk, 0);
+  tb->connect(src, 0, nop, 0);
 }
 
 JNIEXPORT void JNICALL
